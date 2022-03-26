149a45dbe2fb2fe73bb86647b94cdd25ae68e73a
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-03-26 13:05:01 -0300

    fazendo crud

---
+++ teste/src/main/java/com/projeto/teste/controle/Contole.java
@@ -0,0 +1,50 @@
+package com.projeto.teste.controle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.projeto.teste.entidade.Usuario;
+import com.projeto.teste.servico.UsuarioServico;
+
+@Controller
+public class Contole {
+	@Autowired 
+	private UsuarioServico usuarioServico;
+	
+	@GetMapping("/")
+	public String verUsuarios(Model modelo) {
+		modelo.addAttribute("listaUsuarios", usuarioServico.pegarTodosUsuarios());
+		return "index";
+	}
+	
+	@GetMapping("/cadastrarUsuarios")
+	public String cadatrarUsuarios(Model modelo) {
+		Usuario usuario = new Usuario();
+		modelo.addAttribute("usuario", usuario);
+		return "cadastro-usuario";
+	}
+	
+	@PostMapping("/salvarUsuarios")
+	public String salvarUsuarios(@ModelAttribute("usuario") Usuario usuario) {
+		usuarioServico.salvarUsuario(usuario);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/editarUsuarios/{id}")
+	public String editarUsuarios(@PathVariable(value = "id") long id, Model modelo) {
+		Usuario usuario = usuarioServico.pegarUsuarioPorId(id);
+		modelo.addAttribute("usuario", usuario);
+		return "editar-usuario";
+	}
+	
+	@GetMapping("/excluirUsuarios/{id}")
+	public String excluirUsuarios(@PathVariable(value = "id") long id) {
+		usuarioServico.excluirUsuarioPorId(id);
+		return "redirect:/";
+	}
+}
---
+++ teste/src/main/java/com/projeto/teste/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,11 @@
+package com.projeto.teste.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.teste.entidade.Usuario;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
+	
+}
